ced800b6672ad469ff4c64abbc66ff2be9c71185
Farhan Hasin Chowdhury <mail@farhan.info>
2020-12-09 20:06:05 +0600

    Initial Release

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: ["prettier"],
+    extends: [
+      'airbnb-base',
+      'plugin:prettier/recommended'
+    ],
+    env: {
+      node: true,
+    },
+    rules: {
+      "global-require": 0
+    }
+  };
+  
